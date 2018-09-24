c74dc3a580f3508a65e6f7e6709eeb82eb390dad
vflores <valerie.flores64@gmail.com>
2018-09-23 20:39:49 -0700

    updated burger.js with ORM functions, connected controller with burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,22 @@
 var orm = require("../config/orm.js");
 
 //Create code that will call the ORM functions using burger specifi input for the ORM
+var burgers = {
+    selectAll: function(callBack) {
+        orm.selectAll("burgers", function(res) {
+            callBack(res);
+        });
+    },
+    insertOne: function(cols, vals, callBack) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            callBack(res);
+        });
+    },
+    updateOne: function(colVals, cond, callBack) {
+        orm.updateOne("burgers", colVals, cond, function(res) {
+            callBack(res);
+        });
+    }
+};
 
 module.exports = burgers;
